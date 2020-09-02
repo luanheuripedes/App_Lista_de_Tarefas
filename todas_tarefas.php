3c6dd3ef94149d04b37471c24fbe869063e50407
Luan Heuripedes <heuripedesmoreiradepaula@gmail.com>
2020-09-02 17:41:56 -0300

    função de remover

--- todas_tarefas.php
+++ todas_tarefas.php
@@ -68,9 +68,10 @@
 
 				//incluir form na pagina //instrução que permite uma arvore de elementos html seja inserida dentro de um outro elemento ja renderizado FAZER UM INSERT APOS A RENDERIZAÇÃO DA PAGINA
 				tarefa.insertBefore(form, tarefa[0]);
+			}
 
-				
-
+			function remover(id){
+				location.href = 'todas_tarefas.php?acao=remover&id='+id;
 			}
 		</script>
 	</head>
@@ -109,7 +110,7 @@
 											 <?= $tarefa->tarefa ?> (<?= $tarefa->status?>)
 										</div>
 										<div class="col-sm-3 mt-2 d-flex justify-content-between">
-											<i class="fas fa-trash-alt fa-lg text-danger"></i>
+											<i class="fas fa-trash-alt fa-lg text-danger" onclick="remover(<?= $tarefa->id ?>)"></i>
 											<i class="fas fa-edit fa-lg text-info" onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
 											<i class="fas fa-check-square fa-lg text-success"></i>
 										</div>
